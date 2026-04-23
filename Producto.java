b34cb318ff7da11be8f13898a10f672a6f0897e5
sofi450 <karen.diaz07@usa.edu.co>
2026-04-23 07:25:57 -0500

    Add abstract Producto class with properties and methods

---
+++ Producto.java
@@ -0,0 +1,27 @@
+package Empresa;
+
+public abstract class Producto {
+    private int codigo;
+    private String descripcion;
+    private double precio;
+
+    public Producto(int codigo, String descripcion, double precio) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public abstract double calcularPrecioVenta();
+}
